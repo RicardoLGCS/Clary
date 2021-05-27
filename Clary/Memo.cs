f6dabe3cf8139a20cea9bf8c74a4cdc6cb7b9d00
Ricardo Silva <rickysilva2002@hotmail.com>
2021-05-27 22:50:46 +0100

    Created Memo.cs structure

--- Clary/Memo.cs
+++ Clary/Memo.cs
@@ -8,5 +8,18 @@ namespace Clary
 {
     class Memo
     {
+        public string sMemoTitle, sMemoDate;
+
+        string MemoTitle
+        {
+            set { sMemoTitle = value; }
+            get { return sMemoTitle; }
+        }
+
+        string MemoDate
+        {
+            set { sMemoDate = value; }
+            get { return sMemoDate; }
+        }
     }
 }
